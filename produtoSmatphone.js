4fdc9d641254e281ca9684e3c01be471a5eb8591
Silas <139024636+zSilas@users.noreply.github.com>
2023-10-11 01:41:02 +0000

    produto Smartphone

---
+++ produtoSmatphone.js
@@ -0,0 +1,48 @@
+class DispositivoEletronico {
+    constructor(nome) {
+        this.nome = nome;
+        this.ligado = false;
+    }
+
+    ligar() {
+        if (this.ligado) {
+            console.log(this.nome + ' já ligado');
+            return;
+        }
+        this.ligado = true;
+        console.log(this.nome + ' foi ligado');
+    }
+
+    desligar() {
+        if (!this.ligado) {
+            console.log(this.nome + ' já desligado');
+            return;
+        }
+        this.ligado = false;
+        console.log(this.nome + ' foi desligado');
+    }
+}
+
+class Smartphone extends DispositivoEletronico {
+    constructor (nome, cor, modelo){
+        super(nome);
+        this.cor = cor;
+        this.modelo = modelo;
+    }
+}
+
+class Tablet extends DispositivoEletronico {
+    constructor (nome, temWifi) {
+        super(nome);
+        this.temWifi = temWifi;
+    }
+    ligar () {
+        console.log('Olha, você alterou o método ligar.')
+    }
+}
+
+const s1 = new Smartphone('Samsung', 'Preto', 'Galaxy S10')
+console.log(s1)
+
+const t1 = new Tablet('Ipad', true)
+t1.ligar()
